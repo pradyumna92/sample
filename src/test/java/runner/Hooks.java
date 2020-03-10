c1c2ecde68cea25a9d63a8b2a3c125c60cf3c4d9
pradyumna92 <pnkashyap92@gmail.com>
2020-03-10 21:14:41 +0100

    Updated hooks

--- src/test/java/runner/Hooks.java
+++ src/test/java/runner/Hooks.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 public class Hooks {
 
-    String filePath = "C:\\Users\\PRADYUMNA\\IdeaProjects\\FriendSurance\\ScreenShots\\";
+    String filePath = "C:\\Users\\PRADYUMNA\\ScreenShots\\";
     WebDriver driver;
 
     @Before
